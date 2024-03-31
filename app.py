eb4da72e26967db413daff3a2937c51f076d4e5b
Jay Lalwani <lalwanijay2005@gmail.com>
2024-03-31 15:37:31 -0400

    style update

--- app.py
+++ app.py
@@ -1,10 +1,14 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template("index.html")
+
+@app.route('/train')
+def train():
+    return redirect(url_for('index'))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
