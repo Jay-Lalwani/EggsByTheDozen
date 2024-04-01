7c43cbdd21cb15afd30943b064c9248266d5b828
Jay Lalwani <lalwanijay2005@gmail.com>
2024-04-01 17:52:48 -0400

    website update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 
 app = Flask(__name__)
 
@@ -6,9 +6,18 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route('/train')
-def train():
-    return redirect(url_for('index'))
+@app.route('/predict', methods=['POST'])
+def predict():
+    if 'Image-Upload' not in request.files:
+        return redirect(request.url)
+    file = request.files['Image-Upload']
+    if file.filename == '':
+        return redirect(request.url)
+    if file:
+        # Process the file here
+        # For example, save it to a directory or pass it to your prediction model
+        return 'File received and processed'
+    return 'No file received'
 
 if __name__ == '__main__':
     app.run(debug=True, port=5001)
