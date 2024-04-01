de92d96186b5c3b7aeb314ce6715399778d8278d
Jay Lalwani <lalwanijay2005@gmail.com>
2024-04-01 17:38:03 -0400

    Training optimization for M1 mac

--- fecaleggcount.py
+++ fecaleggcount.py
@@ -4,4 +4,4 @@ from ultralytics import YOLO
 model = YOLO("yolov8n.pt")
 
 # Train the model
-results = model.train(data="datasets/eggPretrained/data.yaml", epochs=50, plots=False)
+results = model.train(data="datasets/eggPretrained/data.yaml", epochs=50, plots=False, device='mps')
