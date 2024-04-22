4902d542c7b62db1aa86112e9155c031552c877d
Jay Lalwani <lalwanijay2005@gmail.com>
2024-04-22 19:34:27 -0400

    training setup

--- fecaleggcount.py
+++ fecaleggcount.py
@@ -1,14 +1,14 @@
 from ultralytics import YOLO
 
 
-# model = YOLO("yolov8n.pt")
+model = YOLO("yolov8n.pt")
 
 # Train the model
-# results = model.train(data="datasets/eggPretrained/data.yaml", epochs=50, plots=False, device='mps')
+results = model.train(data="datasets/eggPretrained/data.yaml", epochs=50, plots=False) #, device='mps') #uncomment for M1 Macs
 
 
 # Load the trained model
-model = YOLO("runs/detect/train2/weights/best.pt") # train --> 2 epochs; train2 --> 5 epochs;
+# model = YOLO("runs/detect/train2/weights/best.pt") # train --> 2 epochs; train2 --> 5 epochs;
 
 # Test the model on a single image and add boxes around the detected objects and print the number of objects detected
 # results = model("egg1.png", save=True, exist_ok=True, iou=0)[0]
